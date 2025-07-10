ce88c7f1581c187a5410bf25014dcad0234d76de
Rawlinson Rolim <alemaorolim@gmail.com>
2025-07-10 09:14:38 -0300

    Add load balancer

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,13 +1,18 @@
 
 import express from 'express';
+import os from 'os';
 
 const app = express();
 const port = 3000;
+const hostname = os.hostname();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('API testessss!');
+    res.send({
+        message: 'Acesso a API node',
+        instance: hostname
+    });
 });
 
 app.listen(port, () => {
